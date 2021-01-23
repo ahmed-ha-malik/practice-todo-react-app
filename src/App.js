c944a26644cf023868019c39e018e80c56c85882
Ahmed Malik <ahmed.ha.malik@gmail.com>
2021-01-22 21:20:39 -0500

    Finished displaying each todo onClick of the plus button

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, {useState} from 'react';
+import './App.css';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+
+function App() {
+  const [inputText, setInputText] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Ahmed's Todo List</h1>
+      </header>
+      <TodoForm inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} />
+      <TodoList todos={todos} />
+    </div>
+  );
+}
+
+export default App;
